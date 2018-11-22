4923d1067546ecd4811e5c26e662f6d271656a6d
JackChen <king20boy@gmail.com>
2018-11-22 11:48:42 +0800

    Reformatter code

--- ahbottomnavigation/src/main/java/com/aurelhubert/ahbottomnavigation/AHBottomNavigationItem.java
+++ ahbottomnavigation/src/main/java/com/aurelhubert/ahbottomnavigation/AHBottomNavigationItem.java
@@ -17,136 +17,136 @@ import android.support.v7.content.res.AppCompatResources;
  */
 public class AHBottomNavigationItem {
 
-	private String title = "";
-	private Drawable drawable;
-	private int color = Color.GRAY;
+    private String title = "";
+    private Drawable drawable;
+    private int color = Color.GRAY;
 
-	private
-	@StringRes
-	int titleRes = 0;
-	private
-	@DrawableRes
-	int drawableRes = 0;
-	private
-	@ColorRes
-	int colorRes = 0;
+    private
+    @StringRes
+    int titleRes = 0;
+    private
+    @DrawableRes
+    int drawableRes = 0;
+    private
+    @ColorRes
+    int colorRes = 0;
 
-	/**
-	 * Constructor
-	 *
-	 * @param title    Title
-	 * @param resource Drawable resource
-	 */
-	public AHBottomNavigationItem(String title, @DrawableRes int resource) {
-		this.title = title;
-		this.drawableRes = resource;
-	}
+    /**
+     * Constructor
+     *
+     * @param title    Title
+     * @param resource Drawable resource
+     */
+    public AHBottomNavigationItem(String title, @DrawableRes int resource) {
+        this.title = title;
+        this.drawableRes = resource;
+    }
 
-	/**
-	 * @param title    Title
-	 * @param resource Drawable resource
-	 * @param color    Background color
-	 */
-	@Deprecated
-	public AHBottomNavigationItem(String title, @DrawableRes int resource, @ColorRes int color) {
-		this.title = title;
-		this.drawableRes = resource;
-		this.color = color;
-	}
+    /**
+     * @param title    Title
+     * @param resource Drawable resource
+     * @param color    Background color
+     */
+    @Deprecated
+    public AHBottomNavigationItem(String title, @DrawableRes int resource, @ColorRes int color) {
+        this.title = title;
+        this.drawableRes = resource;
+        this.color = color;
+    }
 
-	/**
-	 * Constructor
-	 *
-	 * @param titleRes    String resource
-	 * @param drawableRes Drawable resource
-	 * @param colorRes    Color resource
-	 */
-	public AHBottomNavigationItem(@StringRes int titleRes, @DrawableRes int drawableRes, @ColorRes int colorRes) {
-		this.titleRes = titleRes;
-		this.drawableRes = drawableRes;
-		this.colorRes = colorRes;
-	}
+    /**
+     * Constructor
+     *
+     * @param titleRes    String resource
+     * @param drawableRes Drawable resource
+     * @param colorRes    Color resource
+     */
+    public AHBottomNavigationItem(@StringRes int titleRes, @DrawableRes int drawableRes, @ColorRes int colorRes) {
+        this.titleRes = titleRes;
+        this.drawableRes = drawableRes;
+        this.colorRes = colorRes;
+    }
 
-	/**
-	 * Constructor
-	 *
-	 * @param title    String
-	 * @param drawable Drawable
-	 */
-	public AHBottomNavigationItem(String title, Drawable drawable) {
-		this.title = title;
-		this.drawable = drawable;
-	}
+    /**
+     * Constructor
+     *
+     * @param title    String
+     * @param drawable Drawable
+     */
+    public AHBottomNavigationItem(String title, Drawable drawable) {
+        this.title = title;
+        this.drawable = drawable;
+    }
 
-	/**
-	 * Constructor
-	 *
-	 * @param title    String
-	 * @param drawable Drawable
-	 * @param color    Color
-	 */
-	public AHBottomNavigationItem(String title, Drawable drawable, @ColorInt int color) {
-		this.title = title;
-		this.drawable = drawable;
-		this.color = color;
-	}
+    /**
+     * Constructor
+     *
+     * @param title    String
+     * @param drawable Drawable
+     * @param color    Color
+     */
+    public AHBottomNavigationItem(String title, Drawable drawable, @ColorInt int color) {
+        this.title = title;
+        this.drawable = drawable;
+        this.color = color;
+    }
 
-	public String getTitle(Context context) {
-		if (titleRes != 0) {
-			return context.getString(titleRes);
-		}
-		return title;
-	}
+    public String getTitle(Context context) {
+        if (titleRes != 0) {
+            return context.getString(titleRes);
+        }
+        return title;
+    }
 
-	public void setTitle(String title) {
-		this.title = title;
-		this.titleRes = 0;
-	}
+    public void setTitle(String title) {
+        this.title = title;
+        this.titleRes = 0;
+    }
 
-	public void setTitle(@StringRes int titleRes) {
-		this.titleRes = titleRes;
-		this.title = "";
-	}
+    public void setTitle(@StringRes int titleRes) {
+        this.titleRes = titleRes;
+        this.title = "";
+    }
 
-	public int getColor(Context context) {
-		if (colorRes != 0) {
-			return ContextCompat.getColor(context, colorRes);
-		}
-		return color;
-	}
+    public int getColor(Context context) {
+        if (colorRes != 0) {
+            return ContextCompat.getColor(context, colorRes);
+        }
+        return color;
+    }
 
-	public void setColor(@ColorInt int color) {
-		this.color = color;
-		this.colorRes = 0;
-	}
+    public void setColor(@ColorInt int color) {
+        this.color = color;
+        this.colorRes = 0;
+    }
 
-	public void setColorRes(@ColorRes int colorRes) {
-		this.colorRes = colorRes;
-		this.color = 0;
-	}
+    public void setColorRes(@ColorRes int colorRes) {
+        this.colorRes = colorRes;
+        this.color = 0;
+    }
 
-	public int getDrawableRes() {
-		return drawableRes;
-	}
+    public int getDrawableRes() {
+        return drawableRes;
+    }
 
-	public Drawable getDrawable(Context context) {
-		if (drawableRes != 0) {
-			try {
-				return AppCompatResources.getDrawable(context, drawableRes);
-			} catch (Resources.NotFoundException e) {
-				return ContextCompat.getDrawable(context, drawableRes);
-			}
-		}
-		return drawable;
-	}
+    public Drawable getDrawable(Context context) {
+        if (drawableRes != 0) {
+            try {
+                return AppCompatResources.getDrawable(context, drawableRes);
+            } catch (Resources.NotFoundException e) {
+                return ContextCompat.getDrawable(context, drawableRes);
+            }
+        }
+        return drawable;
+    }
 
-	public void setDrawable(@DrawableRes int drawableRes) {
-		this.drawableRes = drawableRes;
-		this.drawable = null;
-	}
+    public void setDrawable(@DrawableRes int drawableRes) {
+        this.drawableRes = drawableRes;
+        this.drawable = null;
+    }
 
-	public void setDrawable(Drawable drawable) {
-		this.drawable = drawable;
-		this.drawableRes = 0;
-	}
+    public void setDrawable(Drawable drawable) {
+        this.drawable = drawable;
+        this.drawableRes = 0;
+    }
 }
